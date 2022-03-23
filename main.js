28277bf934afa0e12b09aa3ce018d6b7c17c6586
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-03-23 18:57:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ Webcam.set({
     image_format: 'png',
     png_quality: 90
 });
-camera = document.getElementById("webcam");
-Webcam.attach(camera);
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
 
 function capture(){
     Webcam.snap(function(data_uri) {
@@ -24,4 +24,61 @@ function speak(){
     var utterThis= new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
-/The Names for the gestures are Three Fingers, Thumbs Up, Thumbs Down, Two fingers, and Fingers Down/
+//The Names for the gestures are Three Fingers, Thumbs Up, Thumbs Down, Two fingers, and Fingers Down//
+function identify(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("prediction_1").innerHTML= results[0].label;
+        document.getElementById("prediction_2").innerHTML= results[1].label;
+        prediction_1= results[0].label;
+        prediction_2= results[1].label;
+        speak();
+        if (results[0].label == "Three Fingers"){
+            document.getElementById("meaning").innerHTML= 'Three fingers means "This is looking amazing".';
+            document.getElementById("hand_gesture_emoji_1").innerHTML= "&#128076"; 
+        }
+        if (results[0].label == "Thumbs Up"){
+            document.getElementById("meaning").innerHTML= 'Thumbs up means "Good job, all the best, and okay".';
+            document.getElementById("hand_gesture_emoji_1").innerHTML= "&#128077;"; 
+        }
+        if (results[0].label == "Thumbs Down"){
+            document.getElementById("meaning").innerHTML= 'Thumbs down means "No, disapproval, or rejection".';
+            document.getElementById("hand_gesture_emoji_1").innerHTML= "&#128078;"; 
+        }
+        if (results[0].label == "Two Fingers"){
+            document.getElementById("meaning").innerHTML= 'Two fingers means "That was a great victory".';
+            document.getElementById("hand_gesture_emoji_1").innerHTML= "&#9996;"; 
+        }
+        if (results[0].label == "Fingers Down"){
+            document.getElementById("meaning").innerHTML= 'Fingers down or fist represents "resistance or a fist bumb".';
+            document.getElementById("hand_gesture_emoji_1").innerHTML= "&#9994;"; 
+        }
+//meanings 2 here and bellow//
+        if (results[1].label == "Three Fingers"){
+            document.getElementById("meaning_2").innerHTML= 'Three fingers means "This is looking amazing".';
+            document.getElementById("hand_gesture_emoji_2").innerHTML= "&#128076"; 
+        }
+        if (results[1].label == "Thumbs Up"){
+            document.getElementById("meaning_2").innerHTML= 'Thumbs up means "Good job, all the best, and okay".';
+            document.getElementById("hand_gesture_emoji_2").innerHTML= "&#128077;"; 
+        }
+        if (results[1].label == "Thumbs Down"){
+            document.getElementById("meaning_2").innerHTML= 'Thumbs down means "No, disapproval, or rejection".';
+            document.getElementById("hand_gesture_emoji_2").innerHTML= "&#128078;"; 
+        }
+        if (results[1].label == "Two Fingers"){
+            document.getElementById("meaning_2").innerHTML= 'Two fingers means "That was a great victory".';
+            document.getElementById("hand_gesture_emoji_2").innerHTML= "&#9996;"; 
+        }
+        if (results[1].label == "Fingers Down"){
+            document.getElementById("meaning_2").innerHTML= 'Fingers down or fist represents "resistance or a fist bumb".';
+            document.getElementById("hand_gesture_emoji_2").innerHTML= "&#9994;"; 
+        }
+    }
+}
